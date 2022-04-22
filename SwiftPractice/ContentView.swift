60c4c5f91db879d7027ea29f6f153c18f5c65623
张奎 <evija98@icloud.com>
2022-04-22 15:55:56 +0800

    update
    1.add Demo

---
+++ SwiftPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftPractice
+//
+//  Created by 98 Lotus on 2022/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftPractice/SwiftPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPracticeApp.swift
+//  SwiftPractice
+//
+//  Created by 98 Lotus on 2022/4/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
